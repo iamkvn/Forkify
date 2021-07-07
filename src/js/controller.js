85483d7d855d1a527949a96c525af40e99853d12
iamkevn <iamkevnrodriguez@gmail.com>
2021-07-06 22:25:06 -0400

    setting up parcel

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,18 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const log = console.log;
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+    log(res, data);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
